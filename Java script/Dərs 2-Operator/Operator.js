cab928c5d9314d4f57cc388f4abe2fe8da28e679
NihadMahmudov <mahmudovnihad537@gmail.com>
2026-03-10 18:30:31 +0400

    Ders-2

---
+++ Java script/Dərs 2-Operator/Operator.js
@@ -0,0 +1,68 @@
+//1) Math operator
+// + ; - ; / ; x++ ; ++x ; --x; x-- ; %
+
+/*
+let a=5
+let b=++a
+console.log(b)//olar
+*/
+
+
+/*
+let a=5
+let b=++a
+console.log(b)//olmaz
+*/
+
+//- operatoruda eynidi
+
+//2) Assigment operator
+// =+ ; -= ; &= ; %= ; **=
+
+/*
+let a=5
+let b= a+=1
+console.log(b)
+// - verilen ededi cixiriq % modunu tapib menimsedirik ** quvveti
+*/
+
+// 3)Comparisons operator
+//= == === != !== < > >= <=
+/*let a=1,b=2
+console.loq(a>b)
+console.loq(a<b)
+console.loq(a=b)
+console.loq(a==b)
+console.loq(a===b)
+console.loq(a>=b)
+console.loq(a<=b)*/
+
+//4)Locikal operator
+// & || !
+/*let a=1,b=2
+(a>b)||(b=a)// false Or'da bir true varsa hamisi truse
+(a=b)&(b>a)//false and' da bir dene false varsa hamisi false
+!(a=b)// not neticeni tersine cevirir
+*/
+
+//5)Conditional operator
+/*let a=2,b=3
+let netice= a==b?("Salam"):a>b?("sagol"):(".")
+console.loq(netice)
+*/
+
+//6)Type of operator
+/*let x=3;
+console.loq(typeof(x))
+*/
+
+//7)String operator
+/*let a=5
+let b="5"
+console.loq(a+b)*/
+
+//8) Spread operator
+/*let arr1=[1,2,3]
+let arr2=[...arr1 ,3,4,5]
+console.loq(arr2)
+*/
